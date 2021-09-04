23cb2a7e4daa088d6638a7383a5ac1eb5dce51e0
NikiTricky <63540250+NikiTricky2@users.noreply.github.com>
2021-09-04 14:29:16 +0300

    Update dataset_parsing.py

--- pedict0r/dataset_parsing.py
+++ pedict0r/dataset_parsing.py
@@ -4,7 +4,11 @@ import numpy as np
 def load(path):
   return pd.read_csv(path)
 
+def save(df, path):
+  return df.to_csv(path)
+
 def column_types(df):
   col = list(df.columns)
-  types = [typeof it for it in list(df.iloc[0])]
+  types = [type(it) for it in list(df.iloc[0])]
   return zip(col, types)
+
